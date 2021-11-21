aa999e38c78f48b6fdd0dd14deb8f22655f936ba
Sandi Andrian <andrian.sandi@maleo.agency>
2021-11-21 17:58:22 +0700

    Testing using Prisma ORM

--- index.ts
+++ index.ts
@@ -1,10 +1,36 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
+// Initialize Express
 const app = express()
 const PORT = 8000
 
-app.get('/', (req, res) => {
-    res.send('Express + Typescript Server')
+// Initialize Prisma
+const prisma = new PrismaClient()
+
+app.get('/', async (req, res) => {
+    await prisma.user.create({
+        data: {
+            name: 'Sandi',
+            email: 'andrian.sandi@gmail.com',
+            jobTitle: 'Technical Lead',
+            posts: {
+                create: { title: 'Hello World'}
+            },
+            profile: {
+                create: { bio: 'I like turtles' }
+            }
+        }
+    })
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true
+        }
+    })
+
+    res.status(200).json(allUsers)
 })
 
 // Booting the server

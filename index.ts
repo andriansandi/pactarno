b71bd3b8d6b85382563e7824b1c509f2384efa13
Sandi Andrian <andrian.sandi@maleo.agency>
2021-11-22 00:09:08 +0700

    add env example and port

--- index.ts
+++ index.ts
@@ -3,34 +3,13 @@ import { PrismaClient } from '@prisma/client'
 
 // Initialize Express
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT
 
 // Initialize Prisma
 const prisma = new PrismaClient()
 
 app.get('/', async (req, res) => {
-    await prisma.user.create({
-        data: {
-            name: 'Sandi',
-            email: 'andrian.sandi@gmail.com',
-            jobTitle: 'Technical Lead',
-            posts: {
-                create: { title: 'Hello World'}
-            },
-            profile: {
-                create: { bio: 'I like turtles' }
-            }
-        }
-    })
-
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-            profile: true
-        }
-    })
-
-    res.status(200).json(allUsers)
+    res.send('Express + Typescript Server')
 })
 
 // Booting the server
